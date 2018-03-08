6b4b749ebf0b75750fe002bad4dfe0fbefa82de6
myersal <myersal@rose-hulman.edu>
2018-03-08 18:46:11 -0500

    Finished Apple Picker

---
+++ Apple Picker/Assets/AppleTree.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+    // Speed at which the AppleTree moves
+    public float speed = 10f;
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 20f;
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+    void Start()
+    {
+        // Dropping apples every second
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    { // b
+        GameObject apple = Instantiate<GameObject>(applePrefab); // c
+        apple.transform.position = transform.position; // d
+        Invoke("DropApple", secondsBetweenAppleDrops); // e
+    }
+
+    void Update()
+    {
+        // Basic Movement
+        Vector3 pos = transform.position; // b
+        pos.x += speed * Time.deltaTime; // c
+        transform.position = pos; // d
+                                  // Changing Direction
+                                  // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Changing Direction Randomly is now time-based because of
+        if (Random.value < chanceToChangeDirections)
+        { // b
+            speed *= -1; // Change direction
+        }
+    }
+}
